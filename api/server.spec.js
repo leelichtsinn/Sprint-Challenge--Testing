c13a366f0208e0557a8c85887eb0aee39a245423
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-02-09 13:57:45 -0500

    added test for / endpoint

---
+++ api/server.spec.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+
+const server = require('./server.js');
+
+describe('the route handlers', () => {
+  describe('get /', () => {
+    it('responds with 200 status', async () => {
+      const response = await request(server).get('/');
+      expect(response.status).toBe(200);
+    });
+
+    it('responds with json', async () => {
+      const response = await requrest(server).get('/');
+      expect(response.type).toMatch(/json/i);
+    });
+
+    it('sends correct response object', async () => {
+      const response = await requrest(server).get('/');
+      expect(response.body).toEqual({ api: 'up and running' });
+    });
+
+  });
+
+});
